2cd05df177e936da42ad4a00e1115401cf1ad21a
bw_flow09 <viethoang.ma1009@gmail.com>
2021-05-27 23:29:50 +0700

    Add service find intersection (block and line)

--- src/service/Math_Cal_Service.java
+++ src/service/Math_Cal_Service.java
@@ -1,6 +1,10 @@
 package service;
 
+import java.util.List;
+
+import georegression.struct.line.LineParametric3D_F32;
 import georegression.struct.point.Point3D_F32;
+import model.Block;
 
 public interface Math_Cal_Service {
 	
@@ -9,5 +13,7 @@ public interface Math_Cal_Service {
 	
 	// Calculate volume rectangular prism
 	float volumeRecCal(Point3D_F32[] coordinates);
+	
+	List<Point3D_F32> getInterBlockAndSegment(Block block, LineParametric3D_F32 line);
 	
 }
--- src/service/impl/Math_Cal_Service_Impl.java
+++ src/service/impl/Math_Cal_Service_Impl.java
@@ -1,8 +1,15 @@
 package service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import georegression.metric.Intersection3D_F32;
+import georegression.struct.line.LineParametric3D_F32;
 import georegression.struct.line.LineSegment3D_F32;
+import georegression.struct.plane.PlaneNormal3D_F32;
 import georegression.struct.point.Point3D_F32;
 import georegression.struct.point.Vector3D_F32;
+import model.Block;
 import service.Math_Cal_Service;
 
 public class Math_Cal_Service_Impl implements Math_Cal_Service{
@@ -55,6 +62,75 @@ public class Math_Cal_Service_Impl implements Math_Cal_Service{
 		return a.getLength()*b.getLength()*c.getLength();
 	}
 
-	 
-
+	@Override
+	public List<Point3D_F32> getInterBlockAndSegment(Block block, LineParametric3D_F32 line) {
+		
+		List<Point3D_F32> results = new ArrayList<>();
+		
+		Point3D_F32[] coors = block.getCoordinates();
+		
+		Vector3D_F32 v1 = new Vector3D_F32(coors[0], coors[1]);
+		Vector3D_F32 v2 = new Vector3D_F32(coors[0], coors[3]);
+		Vector3D_F32 v3 = new Vector3D_F32(coors[0], coors[4]);
+		
+		Point3D_F32 inter1 = new Point3D_F32();
+		PlaneNormal3D_F32 m1 = new PlaneNormal3D_F32(coors[0], v1);
+		if (Intersection3D_F32.intersection(m1, line, inter1)) {
+			if (coors[0].y <= inter1.y && inter1.y <= coors[3].y) {
+				if (coors[0].z < inter1.z && inter1.z <= coors[4].z) {
+					results.add(inter1);
+				}
+			}
+		}
+		
+		Point3D_F32 inter2 = new Point3D_F32();
+		PlaneNormal3D_F32 m2 = new PlaneNormal3D_F32(coors[1], v1);
+		if (Intersection3D_F32.intersection(m2, line, inter2)) {
+			if (coors[1].y <= inter2.y && inter2.y <= coors[2].y) {
+				if (coors[1].z <= inter2.z && inter2.z <= coors[5].z) {
+					results.add(inter2);
+				}
+			}
+		}
+		
+		Point3D_F32 inter3 = new Point3D_F32();
+		PlaneNormal3D_F32 m3 = new PlaneNormal3D_F32(coors[0], v2);
+		if (Intersection3D_F32.intersection(m3, line, inter3)) {
+			if (coors[0].x <= inter3.x && inter3.x <= coors[1].x) {
+				if (coors[0].z <= inter3.z && inter3.z <= coors[4].z) {
+					results.add(inter3);
+				}
+			}
+		}
+		
+		Point3D_F32 inter4 = new Point3D_F32();
+		PlaneNormal3D_F32 m4 = new PlaneNormal3D_F32(coors[3], v2);
+		if (Intersection3D_F32.intersection(m4, line, inter4)) {
+			if (coors[3].x <= inter4.x && inter4.x <= coors[2].x) {
+				if (coors[3].z <= inter4.z && inter4.z <= coors[7].z) {
+					results.add(inter4);
+				}
+			}
+		}
+		
+		Point3D_F32 inter5 = new Point3D_F32();
+		PlaneNormal3D_F32 m5 = new PlaneNormal3D_F32(coors[0], v3);
+		if (Intersection3D_F32.intersection(m5, line, inter5)) {
+			if (coors[0].x <= inter5.x && inter5.x <= coors[1].x) {
+				if (coors[0].y <= inter5.y && inter5.y <= coors[3].y) {
+					results.add(inter5);
+				}
+			}
+		}
+		Point3D_F32 inter6 = new Point3D_F32();
+		PlaneNormal3D_F32 m6 = new PlaneNormal3D_F32(coors[4], v3);
+		if (Intersection3D_F32.intersection(m6, line, inter6)) {
+			if (coors[4].x <= inter6.x && inter6.x <= coors[5].x) {
+				if (coors[4].y <= inter6.y && inter6.y <= coors[7].y) {
+					results.add(inter6);
+				}
+			}
+		}
+		return results;
+	}
 }
